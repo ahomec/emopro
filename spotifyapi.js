191c121f824243c06b4dfe452bd8a636b217249c
ahomec <ahomecgoy@gmail.com>
2023-09-25 16:20:18 -0500

    Setting up Spotify API

---
+++ spotifyapi.js
@@ -0,0 +1,26 @@
+const clientId = "c667d9211520403ca4151abd75314625"; // Replace with your client ID
+const code = undefined;
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
+
+async function redirectToAuthCodeFlow(clientId) {
+    // TODO: Redirect to Spotify authorization page
+}
+
+async function getAccessToken(clientId, code) {
+  // TODO: Get access token for code
+}
+
+async function fetchProfile(token) {
+    // TODO: Call Web API
+}
+
+function populateUI(profile) {
+    // TODO: Update UI with profile data
+}
