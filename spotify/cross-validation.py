ded33eaaa4341c14ace4f7695830711a9479d62b
ahomec <ahomecgoy@gmail.com>
2023-12-07 04:15:52 -0600

    improve code readbility

--- spotify/cross-validation.py
+++ spotify/cross-validation.py
@@ -13,8 +13,10 @@ col_features = df.columns[6:-3]
 x = df[col_features]
 y = df["target"]
 
+# Normalize data
 x = MinMaxScaler().fit_transform(x)
 
+# Encode non-numeric data
 encoder = LabelEncoder()
 encoder.fit(y)
 encoded_y = encoder.transform(y)
@@ -24,14 +26,13 @@ x_train, x_test, y_train, y_test = train_test_split(x, encoded_y, test_size = 0.
 data = list(zip(x, y))
 
 # Gaussian kernel width tuning
+# From https://github.com/seho0808/knn_gaussian_medium/blob/master/Medium_KNN.ipynb
 gen_X = np.array(list(range(1,101)))
 gen_Y = np.array(list(range(1,101)))
 
 results = [] # we will store our results here.
 
-for k in range(1, 12): # maximum neighbor is 9 since 9:1 train split
-    
-    # you can create your own list
+for k in range(1, 12): 
     # for w in np.array([0.1, 0.5, 1, 10, 100])
     for w in np.random.randint(5, 100000, 100)/1000:
         
@@ -43,9 +44,8 @@ for k in range(1, 12): # maximum neighbor is 9 since 9:1 train split
             weights = np.exp(-(distances**2)/kernel_width)
             return weights
         
-        # We take average of ten bootstrapped model.
+        # Bootstrap
         for i in range(0,100):
-            # Below is my personal way of setting random state. This makes each run a bootstrapped model.
             X_train, X_test, y_train, y_test = train_test_split(gen_X.reshape(-1,1),
                                                                 gen_Y,
                                                                 test_size=0.1,
@@ -56,9 +56,9 @@ for k in range(1, 12): # maximum neighbor is 9 since 9:1 train split
             mean_absolute_error = np.mean(abs(y_pred-y_test))
             temp_mae.append(mean_absolute_error)
         average_over_ten_runs = np.mean(temp_mae)
-        results.append([k, w, average_over_ten_runs]) # We store our run result.
+        results.append([k, w, average_over_ten_runs])
+        
+# Print resulting arrays
 results = np.array(results)
 results[:,2].min()
 results[results[:,2].argmin()]
-
-# Optimal width = array([2.        , 7.576     , 0.13824287])
--- spotify/spotify-model.py
+++ spotify/spotify-model.py
@@ -17,16 +17,18 @@ col_features = df_moods.columns[6:-3]
 x = df_moods[col_features]
 y = df_moods['mood']
 
+# Normalize data
 x = MinMaxScaler().fit_transform(x)
 
+# Encode non-numeric data
 encoder = LabelEncoder()
 encoder.fit(y)
 encoded_y = encoder.transform(y)
 
-# define gaussian kernel
+# Define gaussian kernel
 # from https://github.com/seho0808/knn_gaussian_medium/blob/master/Medium_KNN.ipynb
 def gaussian_kernel(distances):
-    kernel_width = 2.926 # 7.576 
+    kernel_width = 2.926 # Tuned in cross-validation.py
     weights = np.exp(-(distances**2)/kernel_width)
     return weights
 
@@ -34,30 +36,29 @@ def gaussian_kernel(distances):
 x_train, x_test, y_train, y_test = train_test_split(x, encoded_y, test_size = 0.3, random_state = 12)
 data = list(zip(x, y))
 
-
-# Default weights
+# k-NN with Default weights
 knn_def = KNeighborsClassifier(n_neighbors = 19)
 knn_def.fit(x_train, y_train)
 print(knn_def.score(x_test, y_test)) 
 
-# Inverse weights
+# k-NN with Inverse weights
 knn_inv = KNeighborsClassifier(n_neighbors = 24, weights = 'distance')
 knn_inv.fit(x_train, y_train)
 print(knn_inv.score(x_test, y_test)) 
 
-# Gaussian weights
+# k-NN Gaussian weights
 knn_gk = KNeighborsClassifier(n_neighbors = 19, weights = gaussian_kernel)
 knn_gk.fit(x_train, y_train)
 print(knn_gk.score(x_test,y_test))
 
 # Choosing optimal k value
-# https://www.datacamp.com/tutorial/k-nearest-neighbor-classification-scikit-learn
+# From https://www.datacamp.com/tutorial/k-nearest-neighbor-classification-scikit-learn
 
-
-# Default
+# Set k-value range
 k_values = [i for i in range (1,31)]
-scores = []
 
+# Default weights
+scores = []
 scaler = StandardScaler()
 X = scaler.fit_transform(x)
 
@@ -70,10 +71,8 @@ for k in k_values:
 import seaborn as sns
 from matplotlib import pyplot as plt
 sns.lineplot(x = k_values, y = scores, marker = 'o')
-plt.legend(labels=["default", "inverse", "Gaussian"])
 
-# Inverse
-k_values = [i for i in range (1,31)]
+# Inverse weights
 scores = []
 
 scaler = StandardScaler()
@@ -85,9 +84,8 @@ for k in k_values:
     scores.append(np.mean(score))
 
 sns.lineplot(x = k_values, y = scores, marker = 'o')
-plt.legend(labels=["default", "inverse", "Gaussian"])
 
-# Gaussian
+# Gaussian weights
 k_values = [i for i in range (1,31)]
 scores = []
 
@@ -99,9 +97,10 @@ for k in k_values:
     score = cross_val_score(knn, X, y, cv=5)
     scores.append(np.mean(score))
 
+
+# Visualize accuracy vs. k-values
 sns.lineplot(x = k_values, y = scores, marker = 'o')
 plt.legend(labels=["default", "inverse", "Gaussian"])
-
 plt.xlabel("K Values")
 plt.ylabel("Accuracy Score")
 # %%
